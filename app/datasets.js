1e434fb4529d1f29dc1a60fea5c196204ddd4005
MateuszC100 <mateuszcierniak102@o2.pl>
2016-07-06 13:48:40 +0200

    add datasets.js

--- app/datasets.js
+++ app/datasets.js
@@ -4,60 +4,60 @@ module.exports = {
 
     clamp1: function ()
     {
-        return [];
+        return [3,4];
     },
 
     clamp2: function ()
     {
-        return [];
+        return [8,7,5];
     },
 
     clamp3: function ()
     {
-        return [];
+        return [5,0,10];
     },
 
     inRange1: function ()
     {
-        return [];
+        return [4,3,5];
     },
 
     inRange2: function ()
     {
-        return [];
+        return [3,4,5];
     },
 
     inRange3: function ()
     {
-        return [];
+        return [5,3,4];
     },
 
     inRange4: function ()
     {
-        return [];
+        return [5,7];
     },
 
     inRange5: function ()
     {
-        return [];
+        return [-1,2];
     },
 
     inRange6: function ()
     {
-        return [];
+        return [7,5];
     },
 
     inRange7: function ()
     {
-        return [];
+        return [7,7];
     },
 
     random1: function ()
     {
-        return [];
+        return [2,5,true];
     },
     random2: function ()
     {
-        return [];
+        return [2,8];
     }
 };
